901d83119be83464de634910beb19f4643c5f443
Andrea Bachechi <Bak-2@Air-di-Andrea.lan>
2015-12-01 23:17:50 +0100

    implementazione della funzione help in ylib

--- mylib.c
+++ mylib.c
@@ -1,24 +1,14 @@
 /**
  ** Author: Bachechi Andrea
  **/
-int getServerInfo(struct sockaddr_in *my_addr , int argc , char* argv)
+int helper()
 {
-  //controllo se la lunghezza degli argomeni passati è giusta
-  if (argc<3)
-    {
-      return 0;
-    }
-  else
-    {
-      char*ip;
-      char*port;
-      ip=argv[2];
-      port=argv[3];
-    }
-  //traduco i valori presi in una struttura che rappresenta un indirizzo di rete
-  memset(my_addr,0,sizeof(*my_addr));
-  my_addr->sin_family= AF_INET;
-  my_addr->sin_addr.s_addr = htonl(ip);
-  my_addr->sin_port=htons(port);
-  return 1;
+  printf("sono disponibili i seguenti comandi :\r\n");
+  printf(" * !help --> mostra l'elenco dei comandi disponibili\r\n");
+  printf(" * !who --> mostra l'elenco dei client connessi al server\r\n");
+  printf(" * !connect <nome_client> --> avvia una partita con l'utente nome_client\r\n");
+  printf(" * !disconnect --> disconnette il client dalla partita attuale\r\n");
+  printf(" * !quit --> disconnette il client dal server\r\n");
+  printf(" * !show_map --> mostra la mappa di gioco\r\n");
+  printf(" * !insert <column> --> inserisce il gettone in column (valido solo durante il proprio turno)\r\n");
 }
